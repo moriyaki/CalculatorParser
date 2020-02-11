b68a4a696657ae5b1e879ee602810ac9988bb1cc
moriyaki <moriyaki999@gmail.com>
2020-02-11 11:45:19 +0900

    ParserとNodeクラスの原型記述

--- CalculatorParser/Parser.cs
+++ CalculatorParser/Parser.cs
@@ -1,20 +1,69 @@
+using System;
 using System.Collections.Generic;
 
 namespace CalculatorParser
 {
-    public class ParseNode
+
+    public enum NodeType
     {
-        public TokenType Type;
-        public int Number { get; set; }
-        public List<ParseNode> parse_list = null;
+        PRIORITY_FORMULA,   // 括弧演算
+        MULTIPLICATION,     // 乗除算
+        ADDITION,           // 加減算
+        NUMBER,             // 
+    }
 
-        
-        
+    public abstract class FormulaNode 
+    {
+        public NodeType Node { get; private set; }
+    }
 
+    // 括弧演算クラス
+    public class PriorityFormulaNode : FormulaNode
+    {
+        public List<FormulaNode> Nodes { get; private set; }
+
+        public PriorityFormulaNode(List<FormulaNode> _nodes) => Nodes = _nodes;
+    }
+
+    // 乗除算ノードクラス
+    public class MultiplicatoinNode : FormulaNode
+    {
+        public Token Operator { get; private set; }
+
+        public MultiplicatoinNode(Token _operator) => Operator = _operator;
+    }
+
+    public class AdditionNode : FormulaNode 
+    {
+        public Token Operator { get; private set; }
+
+        public AdditionNode(Token _operator) => Operator = _operator;
+
+    }
+
+    public class NumberNode : FormulaNode
+    {
+        public int Number { get; private set; }
+
+        public NumberNode(int _number) => Number = _number;
     }
 
 
     public class Parser
     {
+        public TokenType Type;
+        public List<FormulaNode> nodes = null;
+
+
+        public bool Parse(List<Token> token) 
+        {
+            // tokenListすべてを検索
+                // 数値なら数値ノードの追加
+                // LPARAM'('なら Parse を再帰読み出し
+                // RPARAM')'なら 再帰呼び出しから抜ける
+                // 演算子なら演算子ノードを追加
+
+            return false;
+        }
     }
 }
--- CalculatorParser/Program.cs
+++ CalculatorParser/Program.cs
@@ -19,165 +19,148 @@ namespace CalculatorParser
         {
             Console.WriteLine("CalculatorParser");
 
-
-            /*
-                        //string expr = "1+2*(3+4)+5*(6*(7+8)+(9+10))";
-                        string expr = "231 +(9832*6232/ (1230-777))-21001";
-                        Console.WriteLine($"{expr} =  114490.538 ...");
-
-                        DateTime start = DateTime.Now;
-
-                        var formula = new FormulaNode();
-                        formula.Parse(expr);
-                        Console.WriteLine(formula + "...");
-
-                        DateTime end = DateTime.Now;
-
-                        Console.WriteLine("Time... " + (end-start).ToString() );
-            */
         }
     }
 
     /*
-        // 数式ノード
-        public class FormulaNode
+    // 数式ノード
+    public class FormulaNode
+    {
+        public TokenType Type;
+        public int Number;
+        public List<FormulaNode> formula_list;
+
+        public FormulaNode()
         {
-            public TokenType Type;
-            public int Number;
-            public List<FormulaNode> formula_list;
-
-            public FormulaNode()
-            {
-                Type = TokenType.EXPRESSION;
-                formula_list = null;
-            }
-
-            // 数式の文字列化
-            public override string ToString()
-            {
-                if (formula_list == null && Type == TokenType.NUBER)
-                {
-                    return Number.ToString();
-                }
-                else
-                {
-                    var expr = new StringBuilder();
-
-                    foreach (var fl in formula_list)
-                    {
-                        expr.Append(" ");
-                        if (fl.Type == TokenType.NUBER)
-                        {
-                            expr.Append(fl.Number);
-                        }
-                        else if (fl.Type == TokenType.EXPRESSION)
-                        {
-                            expr.Append("(");
-                            expr.Append(fl.ToString());
-                            expr.Append(")");
-                        }
-                        else
-                        {
-                            expr.Append(CalculatorParser.operator_dict[fl.Type]);
-                        }
-                    }
-
-                    return expr.ToString();
-                }
-
-            }
-
-            // パーサ呼び出し
-            public void Parse(string expr)
-            {
-                int i = 0;
-                formula_list = CalculatorParser.Parse(expr, ref i);
-            }
-
-
+            Type = TokenType.EXPRESSION;
+            formula_list = null;
         }
 
-        // 数式パーサ
-        static class CalculatorParser
+        // 数式の文字列化
+        public override string ToString()
         {
-            // 演算子種類→演算子文字列の辞書
-            public static Dictionary<TokenType, string> operator_dict = new Dictionary<TokenType, string>() {
-                [TokenType.OPERATOR_PLUS] = "+",
-                [TokenType.OPERATOR_MINUS] = "-",
-                [TokenType.OPERATOR_MULITPLY] = "*",
-                [TokenType.OPERATOR_DIVIDE] = "/",
-            };
-
-            // 演算子文字列→演算子種類の取得
-            public static TokenType GetDictKey(string es)
+            if (formula_list == null && Type == TokenType.NUBER)
             {
-                return operator_dict.First(x => x.Value == es.ToString()).Key;
+                return Number.ToString();
             }
-
-
-            // 数の評価
-            private static int Number(string s, ref int i)
+            else
             {
-                int num = 0;
+                var expr = new StringBuilder();
 
-                do
+                foreach (var fl in formula_list)
                 {
-                    num = num * 10 + s[i++] - '0';
-                }
-                while (i < s.Length && char.IsDigit(s[i]));
-                return num;
-            }
-
-            public static List<FormulaNode> Parse(string s, ref int i)
-            {
-                var fl_list = new List<FormulaNode>();
-                int num;
-
-                while(i < s.Length)
-                {
-                    if (char.IsDigit(s[i]))
+                    expr.Append(" ");
+                    if (fl.Type == TokenType.NUBER)
                     {
-                        num = Number(s, ref i);
-
-                        // 数値をリストに追加
-                        fl_list.Add(new FormulaNode
-                        {
-                            Type = TokenType.NUBER,
-                            Number = num,
-                        });
+                        expr.Append(fl.Number);
+                    }
+                    else if (fl.Type == TokenType.EXPRESSION)
+                    {
+                        expr.Append("(");
+                        expr.Append(fl.ToString());
+                        expr.Append(")");
                     }
                     else
                     {
-                        if (s[i] == '(')
-                        {
-                            i++;
-                            fl_list.Add(new FormulaNode
-                            {
-                                Type = TokenType.EXPRESSION,
-                                formula_list = Parse(s, ref i),
-                            });
-                        }
-                        else if (s[i] == ')')
-                        {
-                            i++;
-                            return fl_list;
-                        }
-                        else
-                        {
-                            if (operator_dict.ContainsValue(s[i].ToString()))
-                            {
-                                // 演算子が見つかった
-                                fl_list.Add(new FormulaNode {
-                                    Type = GetDictKey(s[i].ToString()),
-                                });
-                            }
-                            i++;
-                        }
+                        expr.Append(CalculatorParser.operator_dict[fl.Type]);
                     }
                 }
-                return fl_list;
-            }
-        }
-    */
 
+                return expr.ToString();
+            }
+
+        }
+
+        // パーサ呼び出し
+        public void Parse(string expr)
+        {
+            int i = 0;
+            formula_list = CalculatorParser.Parse(expr, ref i);
+        }
+
+
+    }
+
+    // 数式パーサ
+    static class CalculatorParser
+    {
+        // 演算子種類→演算子文字列の辞書
+        public static Dictionary<TokenType, string> operator_dict = new Dictionary<TokenType, string>() {
+            [TokenType.OPERATOR_PLUS] = "+",
+            [TokenType.OPERATOR_MINUS] = "-",
+            [TokenType.OPERATOR_MULITPLY] = "*",
+            [TokenType.OPERATOR_DIVIDE] = "/",
+        };
+
+        // 演算子文字列→演算子種類の取得
+        public static TokenType GetDictKey(string es)
+        {
+            return operator_dict.First(x => x.Value == es.ToString()).Key;
+        }
+
+
+        // 数の評価
+        private static int Number(string s, ref int i)
+        {
+            int num = 0;
+
+            do
+            {
+                num = num * 10 + s[i++] - '0';
+            }
+            while (i < s.Length && char.IsDigit(s[i]));
+            return num;
+        }
+
+        public static List<FormulaNode> Parse(string s, ref int i)
+        {
+            var fl_list = new List<FormulaNode>();
+            int num;
+
+            while(i < s.Length)
+            {
+                if (char.IsDigit(s[i]))
+                {
+                    num = Number(s, ref i);
+
+                    // 数値をリストに追加
+                    fl_list.Add(new FormulaNode
+                    {
+                        Type = TokenType.NUBER,
+                        Number = num,
+                    });
+                }
+                else
+                {
+                    if (s[i] == '(')
+                    {
+                        i++;
+                        fl_list.Add(new FormulaNode
+                        {
+                            Type = TokenType.EXPRESSION,
+                            formula_list = Parse(s, ref i),
+                        });
+                    }
+                    else if (s[i] == ')')
+                    {
+                        i++;
+                        return fl_list;
+                    }
+                    else
+                    {
+                        if (operator_dict.ContainsValue(s[i].ToString()))
+                        {
+                            // 演算子が見つかった
+                            fl_list.Add(new FormulaNode {
+                                Type = GetDictKey(s[i].ToString()),
+                            });
+                        }
+                        i++;
+                    }
+                }
+            }
+            return fl_list;
+        }
+    }
+    */
 }
